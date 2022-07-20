8b3445690adcba7e01345ec405496f7f1835acfb
Leandro Machado <leandroalvesmachado@gmail.com>
2022-07-20 08:38:19 -0300

    salvando

--- php-conceitos-lidando-com-dados-loops-e-mais/README.md
+++ php-conceitos-lidando-com-dados-loops-e-mais/README.md
@@ -36,3 +36,12 @@
 * [O bloco elseif sempre precisa de um if antes](#)
 * [O bloco else sempre precisa de um if ou elseif antes -O bloco else só executa se nenhuma das condições anteriores forem verdadeiras](#)
 * [Através do && (AND lógico) e do || (OR lógico) podemos avaliar mais de uma condição](#)
+
+## Aula 05 - Repetindo ações
+* [Existem algumas variações de laços, dentre elas:](#)
+* [while que possui uma condição de entrada](#)
+* [do-while que possui uma condição de permanência (sempre executa o bloco pelo menos uma vez)](#)
+* [for que possui a declaração de uma variável, uma condição entrada e o increment](#)
+* [Dentro do bloco do laço podemos pular uma interação com o comando continue](#)
+* [Com o comando break podemos sair do laço](#)
+* [A variável contador normalmente se chama i](#)
--- php-conceitos-lidando-com-dados-loops-e-mais/decisoes.php
+++ php-conceitos-lidando-com-dados-loops-e-mais/decisoes.php
@@ -1,14 +1,25 @@
 <?php
 
-$idade = 21;
-$numeroDePessoas = 1;
+$idade = 17;
+$numeroDePessoas = 2;
 
 echo "Você só pode entrar se tiver a partir de 18 anos ou ";
 echo "a partir de 16 anos acompanhado" . PHP_EOL;
 
+// if ($idade >= 18) {
+//     echo "Você tem $idade anos. Pode entrar sozinho.";
+// } else {
+//     if ($idade >= 16 && $numeroDePessoas > 1) {
+//         echo "Você tem $idade anos e está acompanhado(a), então pode entrar.";
+//     } else {
+//         echo "Você só tem $idade anos. Você não pode entrar";
+//     }
+// }
+
 if ($idade >= 18) {
-    echo "Você tem $idade anos." . PHP_EOL;
-    echo "Pode entrar";
+    echo "Você tem $idade anos. Pode entrar sozinho.";
+} elseif ($idade >= 16 && $numeroDePessoas > 1) {
+    echo "Você tem $idade anos e está acompanhado(a), então pode entrar.";
 } else {
     echo "Você só tem $idade anos. Você não pode entrar";
 }
